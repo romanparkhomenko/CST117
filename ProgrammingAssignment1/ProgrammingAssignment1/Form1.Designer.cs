15492885537dc89748f59b5877d31152429b2483
romanparkhomenko <rsparkhomenko@gmail.com>
2018-07-01 21:18:38 -0600

    ProgrammingAssignment1

---
+++ ProgrammingAssignment1/ProgrammingAssignment1/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ProgrammingAssignment1 {
+    partial class planetaryWeights {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.earthPounds = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mercuryPounds = new System.Windows.Forms.TextBox();
+            this.venusPounds = new System.Windows.Forms.TextBox();
+            this.marsPounds = new System.Windows.Forms.TextBox();
+            this.jupiterPounds = new System.Windows.Forms.TextBox();
+            this.saturnPounds = new System.Windows.Forms.TextBox();
+            this.uranusPounds = new System.Windows.Forms.TextBox();
+            this.neptunePounds = new System.Windows.Forms.TextBox();
+            this.plutoPounds = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.calculateWeight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "What\'s your weight on Earth?";
+            // 
+            // earthPounds
+            // 
+            this.earthPounds.Location = new System.Drawing.Point(232, 52);
+            this.earthPounds.Name = "earthPounds";
+            this.earthPounds.Size = new System.Drawing.Size(129, 20);
+            this.earthPounds.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "pounds";
+            // 
+            // mercuryPounds
+            // 
+            this.mercuryPounds.Location = new System.Drawing.Point(232, 118);
+            this.mercuryPounds.Name = "mercuryPounds";
+            this.mercuryPounds.Size = new System.Drawing.Size(100, 20);
+            this.mercuryPounds.TabIndex = 3;
+            // 
+            // venusPounds
+            // 
+            this.venusPounds.Location = new System.Drawing.Point(232, 155);
+            this.venusPounds.Name = "venusPounds";
+            this.venusPounds.Size = new System.Drawing.Size(100, 20);
+            this.venusPounds.TabIndex = 4;
+            // 
+            // marsPounds
+            // 
+            this.marsPounds.Location = new System.Drawing.Point(232, 192);
+            this.marsPounds.Name = "marsPounds";
+            this.marsPounds.Size = new System.Drawing.Size(100, 20);
+            this.marsPounds.TabIndex = 5;
+            // 
+            // jupiterPounds
+            // 
+            this.jupiterPounds.Location = new System.Drawing.Point(232, 228);
+            this.jupiterPounds.Name = "jupiterPounds";
+            this.jupiterPounds.Size = new System.Drawing.Size(100, 20);
+            this.jupiterPounds.TabIndex = 6;
+            // 
+            // saturnPounds
+            // 
+            this.saturnPounds.Location = new System.Drawing.Point(232, 265);
+            this.saturnPounds.Name = "saturnPounds";
+            this.saturnPounds.Size = new System.Drawing.Size(100, 20);
+            this.saturnPounds.TabIndex = 7;
+            // 
+            // uranusPounds
+            // 
+            this.uranusPounds.Location = new System.Drawing.Point(232, 303);
+            this.uranusPounds.Name = "uranusPounds";
+            this.uranusPounds.Size = new System.Drawing.Size(100, 20);
+            this.uranusPounds.TabIndex = 8;
+            // 
+            // neptunePounds
+            // 
+            this.neptunePounds.Location = new System.Drawing.Point(232, 341);
+            this.neptunePounds.Name = "neptunePounds";
+            this.neptunePounds.Size = new System.Drawing.Size(100, 20);
+            this.neptunePounds.TabIndex = 9;
+            // 
+            // plutoPounds
+            // 
+            this.plutoPounds.Location = new System.Drawing.Point(232, 380);
+            this.plutoPounds.Name = "plutoPounds";
+            this.plutoPounds.Size = new System.Drawing.Size(100, 20);
+            this.plutoPounds.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Your weight on Mercury:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Your weight on Venus:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Your weight on Mars:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Your weight on Jupiter:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(87, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Your weight on Saturn:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(88, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Your weight on Uranus:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(88, 344);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(125, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Your weight on Neptune:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(88, 383);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(108, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Your weight on Pluto:";
+            // 
+            // calculateWeight
+            // 
+            this.calculateWeight.Location = new System.Drawing.Point(232, 78);
+            this.calculateWeight.Name = "calculateWeight";
+            this.calculateWeight.Size = new System.Drawing.Size(187, 23);
+            this.calculateWeight.TabIndex = 19;
+            this.calculateWeight.Text = "Calculate My Solar System Weight!";
+            this.calculateWeight.UseVisualStyleBackColor = true;
+            this.calculateWeight.Click += new System.EventHandler(this.calculateWeight_Click);
+            // 
+            // planetaryWeights
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 419);
+            this.Controls.Add(this.calculateWeight);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.plutoPounds);
+            this.Controls.Add(this.neptunePounds);
+            this.Controls.Add(this.uranusPounds);
+            this.Controls.Add(this.saturnPounds);
+            this.Controls.Add(this.jupiterPounds);
+            this.Controls.Add(this.marsPounds);
+            this.Controls.Add(this.venusPounds);
+            this.Controls.Add(this.mercuryPounds);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.earthPounds);
+            this.Controls.Add(this.label1);
+            this.Name = "planetaryWeights";
+            this.Text = "Planetary Weights";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox earthPounds;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mercuryPounds;
+        private System.Windows.Forms.TextBox venusPounds;
+        private System.Windows.Forms.TextBox marsPounds;
+        private System.Windows.Forms.TextBox jupiterPounds;
+        private System.Windows.Forms.TextBox saturnPounds;
+        private System.Windows.Forms.TextBox uranusPounds;
+        private System.Windows.Forms.TextBox neptunePounds;
+        private System.Windows.Forms.TextBox plutoPounds;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button calculateWeight;
+    }
+}
+
---
+++ ProgrammingAssignment1/ProgrammingAssignment1/Form1.cs
@@ -0,0 +1,53 @@
+﻿/* Roman Parkhomenko CST117 07/01/2018 
+ * The purpose of this assignment is to exemplify ability
+ * to utilize primitive data types and calculations in C#
+ * with a .NET developed form. This project also shows calculation
+ * and string format handling.
+ * All work is my own.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingAssignment1 {
+    public partial class planetaryWeights : Form {
+        public planetaryWeights() {
+            InitializeComponent();
+        }
+
+        private void calculateWeight_Click(object sender, EventArgs e) {
+            try {
+                // Obtain earth weight from input and declare variables for other planet weights.
+                double earthWeight = double.Parse(earthPounds.Text);
+                double mercuryWeight = (earthWeight / 9.81) * 3.7;
+                double venusWeight = (earthWeight / 9.81) * 8.87;
+                double marsWeight = (earthWeight / 9.81) * 3.711;
+                double jupiterWeight = (earthWeight / 9.81) * 24.79;
+                double saturnWeight = (earthWeight / 9.81) * 10.44;
+                double uranusWeight = (earthWeight / 9.81) * 8.69;
+                double neptuneWeight = (earthWeight / 9.81) * 11.15;
+                double plutoWeight = (earthWeight / 9.81) * 0.60;
+
+                // Fill in and format other planetary weights
+                mercuryPounds.Text = mercuryWeight.ToString("n3");
+                venusPounds.Text = venusWeight.ToString("n3");
+                marsPounds.Text = marsWeight.ToString("n3");
+                jupiterPounds.Text = jupiterWeight.ToString("n3");
+                saturnPounds.Text = saturnWeight.ToString("n3");
+                uranusPounds.Text = uranusWeight.ToString("n3");
+                neptunePounds.Text = neptuneWeight.ToString("n3");
+                plutoPounds.Text = plutoWeight.ToString("n3");
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
