d93663c92576883a818d6af69a7bf5cceee40dc2
romanparkhomenko <rsparkhomenko@gmail.com>
2018-07-14 23:24:42 -0600

    Add files via upload

---
+++ ProgrammingAssignment3/ProgrammingProject3/ProgrammingProject3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ProgrammingProject3 {
+    partial class FileProcessor {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.displayBox = new System.Windows.Forms.ListBox();
+            this.readDataButton = new System.Windows.Forms.Button();
+            this.printLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // displayBox
+            // 
+            this.displayBox.FormattingEnabled = true;
+            this.displayBox.Location = new System.Drawing.Point(56, 35);
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(170, 134);
+            this.displayBox.TabIndex = 1;
+            // 
+            // readDataButton
+            // 
+            this.readDataButton.Location = new System.Drawing.Point(56, 192);
+            this.readDataButton.Name = "readDataButton";
+            this.readDataButton.Size = new System.Drawing.Size(170, 41);
+            this.readDataButton.TabIndex = 2;
+            this.readDataButton.Text = "Read Data From File";
+            this.readDataButton.UseVisualStyleBackColor = true;
+            this.readDataButton.Click += new System.EventHandler(this.readDataButton_Click);
+            // 
+            // printLabel
+            // 
+            this.printLabel.AutoSize = true;
+            this.printLabel.Location = new System.Drawing.Point(40, 239);
+            this.printLabel.Name = "printLabel";
+            this.printLabel.Size = new System.Drawing.Size(0, 13);
+            this.printLabel.TabIndex = 3;
+            // 
+            // FileProcessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.printLabel);
+            this.Controls.Add(this.readDataButton);
+            this.Controls.Add(this.displayBox);
+            this.Name = "FileProcessor";
+            this.Text = "File Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ListBox displayBox;
+        private System.Windows.Forms.Button readDataButton;
+        private System.Windows.Forms.Label printLabel;
+    }
+}
+
---
+++ ProgrammingAssignment3/ProgrammingProject3/ProgrammingProject3/Form1.cs
@@ -0,0 +1,131 @@
+﻿/* Roman Parkhomenko CST117 07/14/2018
+ * The purpose of this application is to exemplify usage of loops
+ * in addition to file processing. This application will take words in
+ * an input file and display those words in the form in addition to creating
+ * an output file and writing to it. Loops and string manipulation is utilized 
+ * to change the words to lowercase and find out certain things about them.
+ * All work is my own.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgrammingProject3 {
+    public partial class FileProcessor : Form {
+        public FileProcessor() {
+            InitializeComponent();
+        }
+
+        private void readDataButton_Click(object sender, EventArgs e) {
+
+            StreamReader inputFile;
+            StreamWriter outputFile;
+
+            //open file dialog
+            openFile.ShowDialog();
+            if (openFile.ShowDialog() == DialogResult.OK) {
+                try {
+                    // input should be words.txt file and output file
+                    // is output.txt
+                    inputFile = File.OpenText(openFile.FileName);
+                    outputFile = File.CreateText("output.txt");
+
+                    string randomWord;
+                    string firstWord = "z";
+                    string lastWord = "a";
+                    string longestWord = "";
+                    string vowelWord = "";
+                    int mostVowels = 0;
+
+                    while (!inputFile.EndOfStream) {
+                        // read line from input file
+                        randomWord = inputFile.ReadLine();
+
+                        string lowercaseWord = randomWord.ToLower();
+                        // 1.a) Print lowercase words to display
+                        displayBox.Items.Add(lowercaseWord);
+                        // 1.b) Print lowercase words fo file
+                        outputFile.WriteLine(lowercaseWord);
+
+                        // Check to see if first two letters of word are less than
+                        // existing word, if so, set first word.
+                        if (lowercaseWord[0] <= firstWord[0]) {
+                            firstWord = lowercaseWord;
+                            //if (lowercaseWord[1] <= firstWord[1]) {
+                            //    firstWord = lowercaseWord;
+                            //}
+                        }
+
+                        // Check to see if first two letters of word are greater than
+                        // existing word, if so, set last word.
+                        if (lowercaseWord[0] >= lastWord[0]) {
+                            lastWord = lowercaseWord;
+                            //if (lowercaseWord[1] >= lastWord[1]) {
+                            //    lastWord = lowercaseWord;
+                            //}
+                        }
+
+                        // Check to see if word is longer than existing longest word,
+                        // if so, set new longest word.
+                        if (lowercaseWord.Length > longestWord.Length) {
+                            longestWord = lowercaseWord;
+                        }
+
+                        /* Check to see if word contains vowels and increment counter.
+                         * if the total vowel count is greater than the most vowel count
+                         * set that word to be the vowelWord. 
+                         */
+                        int totalVowels = 0;
+                        for (int i = 0; i < lowercaseWord.Length; i++) {
+                            if (lowercaseWord[i] == 'a' || lowercaseWord[i] == 'e' || lowercaseWord[i] == 'i' || lowercaseWord[i] == 'o' || lowercaseWord[i] == 'u') {
+                                totalVowels++;
+                            }
+                        }
+
+                        if (totalVowels > mostVowels) {
+                            mostVowels = totalVowels;
+                            vowelWord = randomWord;
+                        }
+
+                    }
+
+                    // 2.a) Print first and last words to display
+                    displayBox.Items.Add(firstWord);
+                    displayBox.Items.Add(lastWord);
+                    // 2.b) Print first and last words fo file
+                    outputFile.WriteLine("The first word is " + firstWord + " and the last word is " + lastWord);
+
+                    // 3.a) Print longest word to display
+                    displayBox.Items.Add(longestWord);
+                    // 3.b) Print longest word to file
+                    outputFile.WriteLine("The longest word is " + longestWord);
+
+                    // 4.a) Print longest word to display
+                    displayBox.Items.Add(vowelWord);
+                    // 4.b) Print longest word to file
+                    outputFile.WriteLine("The word with the most vowels is " + vowelWord);
+
+                    inputFile.Close();
+                    outputFile.Close();
+
+                    printLabel.Text = "You can find an output file at Debug/bin";
+
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            } else {
+                MessageBox.Show("Operation canceled.");
+            }
+        }
+    }
+}
