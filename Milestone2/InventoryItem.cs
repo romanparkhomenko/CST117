be6a5f0c1a3706167daf2f2b7f09230f5abac196
romanparkhomenko <rsparkhomenko@gmail.com>
2018-07-22 16:05:11 -0600

    Add files via upload
    Milestone2, CST117

---
+++ Milestone2/InventoryItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory {
+    class InventoryItem {
+        
+        private string _name;
+        private decimal _price;
+        private int _quantity;
+        private DateTime _expiration;
+
+        public string ItemName {
+            get { return _name; }
+            set { _name = value; }
+            }
+        public decimal Price {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int Quantity {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+        public DateTime Expiration {
+            get { return _expiration; }
+            set { _expiration = value; }
+        }
+
+        // default constructor
+        public InventoryItem() {
+            _name = "Lays Chips";
+            _price = 3.99m;
+            _quantity = 10;
+            _expiration = System.DateTime.Now;
+        }
+
+        //constructor
+        public InventoryItem(string name, decimal price, int quantity, DateTime expiration) {
+            this.ItemName = name;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Expiration = expiration;
+        }
+
+        // unique methods
+        public void print() {
+            Console.Write(_name + "\n" + _price + "\n" + _quantity + "\n" + _expiration + "\n");
+        }
+    }
+}
---
+++ Milestone2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory {
+    class Program {
+        static void Main(string[] args) {
+            //create new inventrory item called chips and print
+            InventoryItem chips = new InventoryItem();
+
+            chips.print();
+
+            // for system pause
+            Console.ReadLine();
+        }
+    }
+}
