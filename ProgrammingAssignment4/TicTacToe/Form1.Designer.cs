f0dc23704aeeb83b331f16cff09fd6d8273b127b
romanparkhomenko <rsparkhomenko@gmail.com>
2018-08-05 12:48:07 -0600

    Add files via upload

---
+++ ProgrammingAssignment4/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TicTacToe {
+    partial class TicTacToe {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.resultsDisplayTextbox = new System.Windows.Forms.RichTextBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 89);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 89);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(173, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 89);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 89);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(91, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 89);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(173, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 89);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 89);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(91, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 89);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Open Sans", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(173, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 89);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "1";
+            // 
+            // resultsDisplayTextbox
+            // 
+            this.resultsDisplayTextbox.BackColor = System.Drawing.SystemColors.Control;
+            this.resultsDisplayTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultsDisplayTextbox.Location = new System.Drawing.Point(12, 311);
+            this.resultsDisplayTextbox.Name = "resultsDisplayTextbox";
+            this.resultsDisplayTextbox.Size = new System.Drawing.Size(236, 28);
+            this.resultsDisplayTextbox.TabIndex = 10;
+            this.resultsDisplayTextbox.Text = "";
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(13, 356);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(118, 27);
+            this.newGameButton.TabIndex = 11;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(137, 356);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 27);
+            this.exitButton.TabIndex = 12;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 395);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.resultsDisplayTextbox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TicTacToe";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox resultsDisplayTextbox;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ ProgrammingAssignment4/TicTacToe/Form1.cs
@@ -0,0 +1,119 @@
+﻿/* Roman Parkhomenko CST117 08/04/2018
+ * The purpose of this project is to exemplify usage
+ * of multidimensional arrays and methods. When clicking
+ * New game the program generates a new tic-tac-toe match and
+ * displays the winner being "X", "O" or "Tied".
+ * All work is my own.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe {
+    public partial class TicTacToe : Form {
+        public TicTacToe() {
+            InitializeComponent();
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e) {
+            // Initilize random number object
+            Random rand = new Random();
+
+            //Set size of gameboard
+            const int ROWS = 3;
+            const int COLS = 3;
+
+            // Create a two-dimensional int array.
+            int[,] gameBoard = new int[ROWS, COLS];
+
+            // Fill array with random integers between 0 and 1
+            for (int row = 0; row < ROWS; row++) {
+                for (int col = 0; col < COLS; col++) {
+                    gameBoard[row, col] = rand.Next(2);
+                }
+            }
+
+            // Fill labels with values from array
+            label1.Text = getValue(gameBoard[0, 0]);
+            label2.Text = getValue(gameBoard[0, 1]);
+            label3.Text = getValue(gameBoard[0, 2]);
+
+            label4.Text = getValue(gameBoard[1, 0]);
+            label5.Text = getValue(gameBoard[1, 1]);
+            label6.Text = getValue(gameBoard[1, 2]);
+
+            label7.Text = getValue(gameBoard[2, 0]);
+            label8.Text = getValue(gameBoard[2, 1]);
+            label9.Text = getValue(gameBoard[2, 2]);
+
+            resultsDisplayTextbox.Text = getWinner(gameBoard);
+        }
+
+        // Method to determine the winner
+        public string getWinner(int[,] intArray) {
+            // set default value to -1 which is equal to a tie
+            int winner = -1;
+
+            // Check for horizontal win
+            if ((intArray[0, 0] == intArray[0, 1]) && (intArray[0, 0] == intArray[0, 2])) {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[1, 0] == intArray[1, 1]) && (intArray[1, 0] == intArray[1, 2])) {
+                winner = intArray[1, 0];
+            }
+            if ((intArray[2, 0] == intArray[2, 1]) && (intArray[2, 0] == intArray[2, 2])) {
+                winner = intArray[2, 0];
+            }
+
+
+            // Check for vertical win
+            if ((intArray[0, 0] == intArray[1, 0]) && (intArray[0, 0] == intArray[2, 0])) {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[0, 1] == intArray[1, 1]) && (intArray[0, 1] == intArray[2, 1])) {
+                winner = intArray[0, 1];
+            }
+            if ((intArray[0, 2] == intArray[1, 2]) && (intArray[0, 2] == intArray[2, 2])) {
+                winner = intArray[0, 2];
+            }
+
+
+            // check for diagonal win 
+            if ((intArray[0, 0] == intArray[1, 1]) && (intArray[0, 0] == intArray[2, 2])) {
+                winner = intArray[0, 0];
+            }
+            if ((intArray[0, 2] == intArray[1, 1]) && (intArray[0, 2] == intArray[2, 0])) {
+                winner = intArray[0, 2];
+            }
+
+            // Check winner
+            if (winner == 0) {
+                return "O is the winner";
+            } else if (winner == 1) {
+                return "X is the winner";
+            } else {
+                return "The game was a tie";
+            }
+        } // End of get winner
+
+        // Assign 0 or X for randomized array values
+        public string getValue(int integerValue) {
+            if (integerValue == 0) {
+                return "0";
+            } else {
+                return "X";
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
